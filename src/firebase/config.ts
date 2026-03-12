bffc15c0faef37784d1224f86f75df24e999aa09
SRIHARI <sriharirroverhere@gmail.com>
2026-03-12 04:08:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8556352546-cdcf5",
+  "appId": "1:562183079056:web:1c3fdd4ce92f45ab60c415",
+  "apiKey": "AIzaSyBuKTJYeeO0Wu6eTx5zvm7X-fSiWsvZTDs",
+  "authDomain": "studio-8556352546-cdcf5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "562183079056"
+};
